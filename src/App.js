8ab2aa0db30f08764b6d8ae98156dccbed7eb8d4
kmfoysal <contact.kmfoysal@gmail.com>
2022-09-20 11:59:34 +0600

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import NavBar from './components/navbar/NavBar';
 import Home from './pages/home/Home';
@@ -6,12 +7,14 @@ import VideoDetails from './pages/videoDetails/VideoDetails';
 
 function App() {
   return (
-    <div>
+      <BrowserRouter>
         <NavBar />
-          <Home />
-          <VideoDetails />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos/:videoId" element={<VideoDetails />} />
+        </Routes>
         <Footer />
-    </div>
+      </BrowserRouter>
   );
 }
 
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Search from '../../assets/search.svg'
 
 const NavBar = () => {
     return (
       <nav class="bg-slate-100 shadow-md">
         <div class="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between items-center py-3 ">
-          <a href="/" className="font-bold">
+          <Link to="/" className="font-bold">
             Redux Video App
-          </a>
+          </Link>
           <div class="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
             <form>
               <input
